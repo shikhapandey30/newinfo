61057b715540a9690cfc292efcf86a8064526b55
arvind kushwah <arvindkushwah9@gmail.com>
2019-06-17 14:47:19 +0530

    First commit

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,24 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, or any plugin's
+// vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file. JavaScript code in this file should be added after the last require_* statement.
+//
+// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require rails-ujs
+//= require activestorage
+//= require turbolinks
+//= require js/jquery-1.7.2.min.js
+//= require js/html5.js
+//= require js/main.js
+//= require js/jquery.carouFredSel-6.2.0-packed.js
+//= require js/jquery.touchSwipe.min.js
+//= require js/checkbox.js
+//= require js/radio.js
+//= require js/selectBox.js
+//= require js/jquery.jqzoom-core.js
