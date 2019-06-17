4a61fe86f69a12debb89504f824b775deb4b0320
arvind kushwah <arvindkushwah9@gmail.com>
2019-06-17 20:11:02 +0530

    add

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,7 +92,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false 
   # config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-  config.action_mailer.default_url_options = { host: 'market-theme.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'https://market-theme.herokuapp.com' }
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.perform_deliveries = true
   ActionMailer::Base.raise_delivery_errors = true
