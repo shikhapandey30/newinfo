70f78024b9823ef247a902c107f77fe5663537fa
shikhpandey30 <shikhapandey1998@gmail.com>
2019-07-07 15:03:08 +0530

    add

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_02_052230) do
+ActiveRecord::Schema.define(version: 2019_07_02_134958) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -107,6 +107,7 @@ ActiveRecord::Schema.define(version: 2019_07_02_052230) do
     t.text "review"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image"
   end
 
   create_table "sub_categories", force: :cascade do |t|
